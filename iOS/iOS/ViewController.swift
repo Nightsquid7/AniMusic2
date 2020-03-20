8e4f34f6284e491893f5ee08f50c009994d3e80d
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-03-20 09:31:00 +0900

    added xcode project .gitignore

---
+++ iOS/iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Steven Berkowitz on R 2/03/20.
+//  Copyright © Reiwa 2 nightsquid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("what the fark")
+    }
+
+
+}
+
