9ee9b2f8d185ff045a766db30f2a11b27ad40fff
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-03-24 18:11:35 +0900

    organized folders

---
+++ iOS/iOS/AnimeList/AnimeSeriesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimeSeriesTableViewCell.swift
+//  iOS
+//
+//  Created by Steven Berkowitz on R 2/03/24.
+//  Copyright © Reiwa 2 nightsquid. All rights reserved.
+//
+
+import UIKit
+
+class AnimeSeriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
