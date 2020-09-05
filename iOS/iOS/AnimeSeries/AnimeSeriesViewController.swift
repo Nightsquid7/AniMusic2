1065d9eb204693689a6987478c5fe0a06a8d3536
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-09-06 07:56:03 +0900

    Fixed popoverPresentationController ipad bug

--- iOS/iOS/AnimeSeries/AnimeSeriesViewController.swift
+++ iOS/iOS/AnimeSeries/AnimeSeriesViewController.swift
@@ -141,6 +141,10 @@ class AnimeSeriesViewController: UIViewController {
         let anime = viewModel.anime
         let titleString = "\(anime.name ?? ""): \(song.name ?? "")"
         let ac = UIAlertController(title: titleString, message: "", preferredStyle: .actionSheet)
+        if let popoverPresentationController = ac.popoverPresentationController {
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+        }
         var supportedSources = ["Spotify"]
 
         for source in song.sources {
