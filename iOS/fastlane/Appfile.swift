94841d1a89413e7725987fb3baedc962b269b7fb
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-11-09 15:42:28 +0900

    added fastLane

---
+++ iOS/fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "animusic" } // The bundle identifier of your app
+var appleID: String { return "stevenaaronberkowitz@gmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "118751850" } // App Store Connect Team ID
+var teamID: String { return "E939BBZ6X4" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
